c72123fdd36aff42d86db0fc96adfee432c346cb
Feruz <104253828+neferuz@users.noreply.github.com>
2025-05-19 17:33:18 +0500

    Update create_superuser.py

--- create_superuser.py
+++ create_superuser.py
@@ -1,10 +1,15 @@
-# create_superuser.py
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mylanding.settings')
+django.setup()
+
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("neferuz", "notferuz@gmail.com", "1235804679f")
+    User.objects.create_superuser("neferuz", "admin@example.com", "admin123")
     print("✅ Superuser created")
 else:
     print("ℹ️ Superuser already exists")
