60afcab1596978b9fd875b110ab2a64f74e7d703
Feruz <104253828+neferuz@users.noreply.github.com>
2025-05-19 17:52:01 +0500

    Update settings.py

--- mylanding/settings.py
+++ mylanding/settings.py
@@ -89,9 +89,10 @@ WSGI_APPLICATION = 'mylanding.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'mylanding' / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
